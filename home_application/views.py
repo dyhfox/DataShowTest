ce020ed041cd59acd3a0ba44d19603c70f7a5843
dyhfox <15608186171@163.com>
2017-09-05 16:42:28 +0800

    update

--- home_application/views.py
+++ home_application/views.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from common.mymako import render_mako_context, render_json
-import re
-import paramiko
+
 import os
 def home(request):
     """
@@ -26,31 +25,45 @@ def contactus(request):
 
 
 def getinfo(request):
-    FilePath = 'C:\\Users\\Administrator\\Documents\\Tencent Files\\707257663\\FileRecv\\stats'
+    #FilePath = 'C:\\Users\\Administrator\\Documents\\Tencent Files\\707257663\\FileRecv\\stats'
+    FilePath = '/gamedata/gamedata/'
     FileName = os.listdir(FilePath)
     fullfile = []
+    filedate = []
     for txtfile in FileName:
         fullfile.append(os.path.join(FilePath,txtfile))
-
+        filedate.append(txtfile[6:16])
     serIp = []
     time = []
     numon = []
     numoff = []
+    Anumon = []
+    Anumoff = []
+    Alogin = []
+    Aoffline = []
     date = 0
     for fullpath in fullfile:
         serIp.append([])
         time.append([])
         numon.append([])
         numoff.append([])
+        Anumoff.append([])
+        Anumon.append([])
         f = open(fullpath, "r")
         lines = f.readlines()
         n = 0
         max = 0
         for line in lines:
             fg1 = 0
-            sum = line.find(r'SUM')
-            if sum >= 0:
+            sum1 = line.find(r'SUM')
+            if sum1 >= 0:
                 fg1 = 1
+                head5 = line.find(r'login-')
+                tail5 = line.find(r' ', head5)
+                head6 = line.find(r'offline-')
+                Anumon[date].append(int(line[head5 + 6: tail5]))
+                Anumoff[date].append(int(line[head6 + 8:]))
+
             line = line.rstrip('\n')
             head = line.find(r'>>>')
             tail = line.find(r'=')
@@ -90,6 +103,8 @@ def getinfo(request):
                     numoff[date][serID - 1].append(int(line[head4 + 8:]))
                     # print line[head4+8:]
                     n = n + 1
+        Alogin.append(float(sum(Anumon[date])/len(Anumon[date])))
+        Aoffline.append(float(sum(Anumoff[date])/len(Anumoff[date])))
         date = date + 1
     print serIp
-    return render_json({'time':time,'ip':serIp, 'id':serID, 'login':numon, 'offline':numoff})
+    return render_json({'time':time,'ip':serIp, 'id':serID, 'login':numon, 'offline':numoff, 'Alogin':Alogin, 'Aoffline':Aoffline, 'filename':filedate})
